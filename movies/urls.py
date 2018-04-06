49fdfb54f2521d3beb09488392daf4ce48bb4cc3
Oleg <justnitro26@gmail.com>
2018-04-06 23:21:20 +0300

    added users to movies's templates

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,17 @@
+import sys
+sys.path.append('..')
+
 from django.urls import path
 
 from . import views
+from users import views as v
+
 
 
 urlpatterns = [
     path('', views.index),
-    path('<int:movie_id>', views.movie)
-]
+    path('<int:movie_id>', views.movie),
+    path('', v.index, name='index'),
+    path('login', v.login_view, name='login'),
+    path('logout', v.logout_view, name='logout')
+    ]
--- movies/views.py
+++ movies/views.py
@@ -11,7 +11,8 @@ def login(request):
 
 def index(request):
     context = {
-        'movies': Movie.objects.all
+        'movies': Movie.objects.all,
+        'user': request.user
     }
     return render(request, 'movies/index.html', context)
 
@@ -22,6 +23,7 @@ def movie(request, movie_id):
     except Movie.DoesNotExist:
         raise Http404('Movie does not exist.')
     context = {
-        'movie': movie
+        'movie': movie,
+        'user': request.user
     }
     return render(request, 'movies/movie.html', context)
